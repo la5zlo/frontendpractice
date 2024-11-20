c00132069a71934d765287bcea3470b84c34eac2
la5zlo <bartalaszlo1991@gmail.com>
2024-11-20 10:16:27 +0100

    frontend practice

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <navbar
+      :pages="pages"
+    ></navbar>
+    <router-view></router-view>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+   components: {
+    Navbar,
+   },
+   methods:{
+    pageCreated(pageObj){
+      this.pages.push(pageObj);
+    }
+   }
+}
+</script>
---
+++ src/data.js
@@ -0,0 +1,20 @@
+const pagesKey = 'pages';
+
+let pageJson = localStorage.getItem(pagesKey);
+let pagesStore = JSON.parse(pageJson);
+
+export default {
+    getAllPages() {
+        return pagesStore;
+    },
+
+    getSinglePage(index) {
+        return pagesStore[index];
+    },
+
+    editPage(index, page) {
+        pagesStore[index] = page;
+
+        localStorage.setItem(pagesKey, JSON.stringify(pagesStore));
+    }
+}
